f346569700811022c4d1200a3f2cc6a9f5ec0e39
Alexander Rodionov <alx1dr@gmail.com>
2018-12-28 14:06:03 +0300

    Add files via upload

---
+++ helpers.py
@@ -0,0 +1,27 @@
+from pyspark.sql.types import *
+import os
+
+def make(*args, spark=None):
+    if not os.path.isdir('ml-latest-small'):
+        os.system('wget http://files.grouplens.org/datasets/movielens/ml-latest-small.zip')
+        os.system('unzip ml-latest-small.zip')
+    result = []
+    for arg in args:
+        if arg == 'ratings':
+            ratings_schema = StructType([
+                StructField('user'     , IntegerType()),
+                StructField('movie'    , IntegerType()),
+                StructField('rating'   , DoubleType() ),
+                StructField('timestamp', LongType()   ),
+            ])
+            ratings = spark.read.csv('ml-latest-small/ratings.csv', header=True, schema=ratings_schema).select(['user' , 'movie', 'rating'])
+            result.append(ratings)
+        if arg == 'movies':
+            movies_schema = StructType([
+                StructField('movie' , IntegerType()),
+                StructField('title' , StringType ()),
+                StructField('genres', StringType ()),
+            ])
+            movies  = spark.read.csv('ml-latest-small/movies.csv' , header=True, schema=movies_schema ).select(['movie', 'title', 'genres'])
+            result.append(movies)
+    return tuple(result)
